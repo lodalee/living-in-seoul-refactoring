0260a1482b709451cbd8da3c08d89588f9facd23
chaewon <cwlee4633@naver.com>
2023-08-06 23:39:42 +0900

    git init

---
+++ src/main/java/com/gavoza/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.gavoza.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication //(exclude = SecurityAutoConfiguration.class)
+@EnableJpaAuditing
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
