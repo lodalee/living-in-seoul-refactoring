6e04777b3d84336b06015badcd19b222b53f935c
lodalee <131640279+lodalee@users.noreply.github.com>
2023-08-11 17:20:58 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/gavoza/backend/global/config/WebSecurityConfig.java
+++ src/main/java/com/gavoza/backend/global/config/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/auth/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/auth/**","/hc").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
