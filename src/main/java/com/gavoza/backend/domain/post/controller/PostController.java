26eda530e9f3078852ec170b89cc623f27270914
chaewon <cwlee4633@naver.com>
2023-08-22 17:40:57 +0900

    좋아요수정(유저용과 비회원용 api)

--- src/main/java/com/gavoza/backend/domain/post/controller/PostController.java
+++ src/main/java/com/gavoza/backend/domain/post/controller/PostController.java
@@ -21,7 +21,6 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
-
     //게시글 생성
     @PostMapping
     public MessageResponseDto uploadFile(
@@ -55,27 +54,52 @@ public class PostController {
         return new MessageResponseDto("게시글 삭제 성공");
     }
 
+    //유저 게시글 상세 조회
+    @GetMapping("/like/get/{postId}")
+    public PostResponse getLikeOnePost(@PathVariable("postId") Long postId,
+                                   @AuthenticationPrincipal UserDetailsImpl userDetails){
+        User user = userDetails.getUser();
+        return postService.getLikeOnePost(postId, user);
+    }
+
     //게시글 상세 조회
     @GetMapping("/get/{postId}")
-    public PostResponse getOnePost(@PathVariable("postId") Long postId,
-                                   @RequestParam("userId") Long userId){
-        return postService.getOnePost(postId, userId);
+    public PostResponse getOnePost(@PathVariable("postId") Long postId){
+        return postService.getOnePost(postId);
+    }
+
+
+    //유저 게시글 전체 조회(커뮤티니)
+    @GetMapping("/like/get")
+    public PostListResponse getLikePost(@RequestParam int page,
+                                    @RequestParam int size,
+                                    @AuthenticationPrincipal UserDetailsImpl userDetails){
+        User user = userDetails.getUser();
+        return postService.getLikePost(page-1,size, user);
     }
 
     //게시글 전체 조회(커뮤티니)
     @GetMapping("/get")
     public PostListResponse getPost(@RequestParam int page,
-                                    @RequestParam int size,
-                                    @RequestParam("userId") Long userId){
-        return postService.getPost(page-1,size, userId);
+                                        @RequestParam int size){
+        return postService.getPost(page-1,size);
+    }
+
+    //유저 게시글 검색
+    @GetMapping("/like/get/search")
+    public PostListResponse searchLikePosts(@RequestParam int page,
+                                        @RequestParam int size,
+                                        @RequestParam String keyword,
+                                        @AuthenticationPrincipal UserDetailsImpl userDetails){
+        User user = userDetails.getUser();
+        return postService.searchLikePosts(page, size, keyword, user);
     }
 
     //게시글 검색
     @GetMapping("/get/search")
     public PostListResponse searchPosts(@RequestParam int page,
-                                        @RequestParam int size,
-                                        @RequestParam String keyword,
-                                        @RequestParam Long userId){
-        return postService.searchPosts(page, size, keyword, userId);
+                                            @RequestParam int size,
+                                            @RequestParam String keyword){
+        return postService.searchPosts(page, size, keyword);
     }
 }
