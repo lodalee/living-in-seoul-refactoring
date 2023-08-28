bb4fcbd9cf438bf64c753f5f301959c91d7bb6cb
chaewon <cwlee4633@naver.com>
2023-08-28 11:18:51 +0900

    Comment 페이징 처리 및 postid로 조회

---
+++ src/main/java/com/gavoza/backend/domain/comment/dto/CommentListResponse.java
@@ -0,0 +1,37 @@
+package com.gavoza.backend.domain.comment.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class CommentListResponse {
+    private PostPageable pageable;
+    private List<CommentResponseDto> comments;
+
+    public CommentListResponse(
+            List<CommentResponseDto> comments,
+
+            int totalPages,
+
+            long totalElements,
+
+            int size
+    ){
+        this.comments = comments;
+        this.pageable = new PostPageable(totalPages, totalElements, size);
+    }
+
+    @Getter
+    private class PostPageable {
+        private int totalPages;
+        private long totalElements;
+        private int size;
+
+        public PostPageable(int totalPages, long totalElements, int size) {
+            this.totalPages = totalPages;
+            this.totalElements = totalElements;
+            this.size = size;
+        }
+    }
+}
