645d391cd81e93ecf78829486746c8fc023936bc
000vin000 <Robbie@email.com>
2023-09-01 11:47:57 +0900

    소셜로그인 토큰 만료일 날짜형태 변경

--- src/main/java/com/gavoza/backend/domain/user/social/dto/SocialLoginResponseDto.java
+++ src/main/java/com/gavoza/backend/domain/user/social/dto/SocialLoginResponseDto.java
@@ -1,5 +1,6 @@
 package com.gavoza.backend.domain.user.social.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +12,7 @@ public class SocialLoginResponseDto {
     private String email;
     private String accessToken;
     private String refreshToken;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private Date expirationDate;
     private String message;
 
